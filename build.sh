f2349fe70c476b0c6e44a9d57f50f7b927502a17
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-21 23:14:30 +0530

    ssssssssssss

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 echo " BUILD START"
+python3.10 -m pip install --upgrade pip
 python3.10 -m pip install -r requirements.txt
 python3.10 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
