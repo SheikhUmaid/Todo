b71c7443200e2eecd8f54fe00ae3055ebdc7455e
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2022-12-21 23:04:59 +0530

    jhhhvnnnnnnnnnnnnnnnn

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
 echo " BUILD START"
-python3.7 -m pip install -r requirements.txt
-python3.7 manage.py collectstatic --noinput --clear
+python3.6 -m pip install -r requirements.txt
+python3.6 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
